bb61b7205ea94f08533b4d5873d6a11fed81eb19
TJC games <73426353+TJCgames@users.noreply.github.com>
2022-05-03 20:04:25 +0100

    pain

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf (t)
 	t.window.title = "Objectcell - editor"
 	t.window.resizable = true
-	t.window.fullscreentype = "exclusive"
-	t.window.fullscreen = true
+	t.window.minwidth = 1
+	t.window.minheight = 1
 end
